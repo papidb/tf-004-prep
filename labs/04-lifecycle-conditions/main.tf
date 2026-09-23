634870e46b0e8797d573b874a7703cff0fef3257
Daniel Benjamin <benjamindaniel706@gmail.com>
2026-09-23 10:54:04 +0100

    chore: add lifecycle conditions, validation and checks

--- labs/04-lifecycle-conditions/main.tf
+++ labs/04-lifecycle-conditions/main.tf
@@ -1,7 +1,11 @@
 resource "random_pet" "a" {
-  length = 4
+  length = var.pet_length
   lifecycle {
     create_before_destroy = true
+    postcondition {
+      condition     = length(self.id) > 0
+      error_message = "id must be non-empty"
+    }
   }
 }
 
@@ -10,12 +14,30 @@ resource "random_pet" "b" {
   keepers = {
     "a_id" = random_pet.a.id
   }
+  lifecycle {
+    precondition {
+      condition     = random_pet.a.length >= 2
+      error_message = "id of a has to be greater or equal to 2"
+    }
+  }
 }
 
 resource "random_pet" "c" {
   length     = 2
   depends_on = [random_pet.a]
-  lifecycle {
-    prevent_destroy = true
+}
+
+variable "pet_length" {
+  type = number
+  validation {
+    condition     = var.pet_length >= 1 && var.pet_length <= 5
+    error_message = "Has to be between 1 and 5"
+  }
+}
+
+check "pet_names_distinct" {
+  assert {
+    condition     = random_pet.a.id != random_pet.b.id
+    error_message = "a.id and b.id must be distinct"
   }
 }
